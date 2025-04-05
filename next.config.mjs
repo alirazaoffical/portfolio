5ffe3a556b27bb91af36f38f98eedd74a8bf315f
Ali Raza <ar93090@gmail.com>
2025-04-05 13:42:38 +0500

    Prepare for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "",
-          port: "",
-          pathname: "**",
-        },
-        {
-          protocol: "https",
-          hostname: "",
-          port: "",
-          pathname: "**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    // No need for remotePatterns if using local images
+  },
+};
+
+export default nextConfig;
